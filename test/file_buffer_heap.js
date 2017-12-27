e6b073c119be0962e811d7726e5f835b5a881045
popomore <sakura9515@gmail.com>
2017-12-27 11:16:11 +0800

    f

---
+++ test/file_buffer_heap.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const fs = require('fs');
+const heapdump = require('heapdump');
+const sleep = require('mz-modules/sleep');
+const Logger = require('./index').Logger;
+const FileBufferTransport = require('./index').FileBufferTransport;
+
+const logger = new Logger();
+const transport = new FileBufferTransport({
+  file: 'a.log',
+  level: 'INFO',
+});
+logger.set('file', transport);
+
+async function run() {
+  let count = 0;
+  let writeCount = 0;
+  const start = Date.now();
+
+  const write = fs.write;
+  fs.write = function write(...args) {
+    writeCount++;
+    if (writeCount !== 3) return write.apply(fs, args);
+    const cb = args[args.length - 1];
+    cb(new Error('write error'));
+  };
+  while(Date.now() - start < 1 * 60 * 1000) {
+    logger.info('log %s %s', new Date(), count++);
+    await sleep(1);
+  }
+
+}
+
+run()
+  .then(() => process.exit())
+  .catch(err => console.log(err));
+
+process.once('exit', () => {
+  heapdump.writeSnapshot('logger.heapsnapshot');
+});
