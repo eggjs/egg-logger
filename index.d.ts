9e98fa5eebe9a1e700e20b4b69029bad8b0de3a1
Andy Hanson <anhans@microsoft.com>
2018-10-17 15:04:40 -0700

    Fix implicit-any error for EggLoggers#set

--- index.d.ts
+++ index.d.ts
@@ -120,7 +120,7 @@ export interface EggLoggersOptions {
   eol?: string;
 }
 
-export class EggLoggers extends Map {
+export class EggLoggers extends Map<string, Logger> {
   constructor(options: EggLoggersOptions);
 
   /**
@@ -138,7 +138,7 @@ export class EggLoggers extends Map {
    * @param {String} name - logger name
    * @param {Logger} logger - Logger instance
    */
-  set(name: string, logger: Logger);
+  set(name: string, logger: Logger): this;
 
   [key: string]: any;
 }
