97335e916c3569739aabef0ecbba2b8f686f87d4
TZ <atian25@qq.com>
2018-12-31 14:41:11 +0800

    feat: file path support relative path

--- test/lib/egg/custom_logger.test.js
+++ test/lib/egg/custom_logger.test.js
@@ -1,30 +1,38 @@
 'use strict';
 
-require('should');
-const fs = require('fs');
+const assert = require('assert');
+const { fs } = require('mz');
 const path = require('path');
 const coffee = require('coffee');
-const rimraf = require('rimraf');
+const { rimraf } = require('mz-modules');
 
 describe('test/egg/custom_logger.test.js', () => {
   const loggerFile = path.join(__dirname, '../../fixtures/egg_custom_logger.js');
-  const filepath = path.join(__dirname, '../../fixtures/tmp/a.log');
+  const tmpDir = path.join(__dirname, '../../fixtures/tmp');
+  const filePath = path.join(tmpDir, '/a.log');
 
-  afterEach(() => {
-    rimraf.sync(path.dirname(filepath));
-  });
+  beforeEach(() => rimraf(path.dirname(filePath)));
+  afterEach(() => rimraf(path.dirname(filePath)));
 
-  it('should format work', done => {
+  it('should format work', function*() {
     const options = {
-      file: filepath,
+      file: filePath,
       level: 'WARN',
     };
-    coffee.fork(loggerFile, [ JSON.stringify(options) ])
-      .end(() => {
-        fs.readFileSync(filepath, 'utf8')
-          .should.match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} ERROR \d+ error foo\n/);
-        done();
-      });
+    yield coffee.fork(loggerFile, [ JSON.stringify(options) ]).end();
+    const log = yield fs.readFile(filePath, 'utf-8');
+    assert(log.match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} ERROR \d+ error foo\n/));
   });
 
+  it('should support relative path', function* () {
+    const options = {
+      dir: tmpDir,
+      file: 'relative.log',
+      level: 'WARN',
+    };
+    yield coffee.fork(loggerFile, [ JSON.stringify(options) ]).end();
+
+    const log = yield fs.readFile(path.join(tmpDir, options.file), 'utf-8');
+    assert(log.match(/\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2},\d{3} ERROR \d+ error foo\n/));
+  });
 });
